9216c2a2896a74a5bda34d9b483053de9cf20ad0
MOHD TALHA ZEESHAN <162271322+MohdTalhaZeeshan@users.noreply.github.com>
2024-03-10 13:35:27 +0530

    Update AdminDao.java

--- OnlineShoping/src/main/java/com/jsp/OnlineShoping/dao/AdminDao.java
+++ OnlineShoping/src/main/java/com/jsp/OnlineShoping/dao/AdminDao.java
@@ -1,7 +1,7 @@
 package com.jsp.OnlineShoping.dao;
 
 public interface AdminDao {
-	
+	void adminLogIn(String emailId,String password);
 	void deleteCustomerBasedOnPhoneNumber(String mobileNumber);
 	void selectCustomerInformation(String name);
 	void purchasedListOfCustomerBasedOnMobileNumber(String mobileNumber);
